0dfe580e0cc9fc1c4ce5e63fadb742336bc06e5b
Hanna Pokamiestova <105422641+HannaPokamiestova@users.noreply.github.com>
2023-04-20 14:13:09 +0200

    Update task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,4 @@
- const ref = {
+const ref = {
   input: document.querySelector("#name-input"),
   output: document.querySelector("#name-output"),
 };
@@ -6,3 +6,4 @@ ref.input.addEventListener("input", onInputChange);
 function onInputChange(event) {
   const inputValue = event.target.value.trim();
   ref.output.textContent = inputValue ? inputValue : "Anonymous";
+}
