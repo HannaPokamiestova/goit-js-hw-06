9773407bb3a888a7e74c0eddcb57785bd7dadcaa
Hanna Pokamiestova <105422641+HannaPokamiestova@users.noreply.github.com>
2023-04-25 13:04:09 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,40 @@
-const form = document.querySelector(".login-form");
-const useremail = document.querySelector('[type="email"]');
-const password = document.querySelector('[type="password"]');
-form.addEventListener("submit", (event) => {
+const formRef = document.querySelector(".login-form");
+
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
   event.preventDefault();
-  const elements = {
-    email: useremail.value,
-    pass: password.value,
-  };
-  if (useremail.value === "" || password.value === "") {
-    return alert("Все поля должны быть заполнены!");
+
+  const elementsRef = event.currentTarget.elements;
+
+  const email = elementsRef.email.value;
+  const password = elementsRef.password.value;
+
+  if (email === "" || password === "") {
+    alert("Please fill in all the fields!");
   }
-  console.log(elements);
-  form.reset();
-});
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
+
+// const form = document.querySelector(".login-form");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const elements = {
+//     email: useremail.value,
+//     pass: password.value,
+//   };
+//   if (useremail.value === "" || password.value === "") {
+//     return alert("Все поля должны быть заполнены!");
+//   }
+//   console.log(elements);
+//   form.reset();
+// });
